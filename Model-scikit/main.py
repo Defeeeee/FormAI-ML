9f7613ae53ae69fd42f04ce17faa88c465a624e3
Federico Diaz Nemeth <fdiaznemeth@gmail.com>
2024-08-26 10:33:31 -0300

    feat(files): Changed main to match ipynb

--- Model-scikit/main.py
+++ Model-scikit/main.py
@@ -1,9 +1,11 @@
+#%% md
+# ### 1.0 Importing Libraries
 #%%
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-
-from sklearn.linear_model import LogisticRegression
+#%% md
+# ### 1.1 Loading the datasets
 #%%
 archivo_url = 'https://raw.githubusercontent.com/NgoQuocBao1010/Exercise-Correction/main/core/plank_model/train.csv'
 
@@ -12,8 +14,6 @@ archivo = 'train.csv'
 ds = pd.read_csv(archivo)
 
 ds
-    
-    
 #%%
 archivo_url1 = 'https://raw.githubusercontent.com/NgoQuocBao1010/Exercise-Correction/main/core/plank_model/test.csv'
 
@@ -22,42 +22,23 @@ archivo = 'test.csv'
 ds_test = pd.read_csv(archivo)
 
 ds_test
+#%% md
+# ### 1.2 Dataset preprocessing
+# Replacing high back and low back with 2 and 1 respectively and correct with 0
 #%%
-ds_test['label'] = ds_test['label'].replace('H', 0)
-ds_test['label'] = ds_test['label'].replace('L', 0)
-ds_test['label'] = ds_test['label'].replace('C', 1)
+ds_test['label'] = ds_test['label'].replace('H', 2)
+ds_test['label'] = ds_test['label'].replace('L', 1)
+ds_test['label'] = ds_test['label'].replace('C', 0)
 #%%
-IMPORTANT_LMS = [
-    "NOSE",
-    "LEFT_SHOULDER",
-    "RIGHT_SHOULDER",
-    "LEFT_ELBOW",
-    "RIGHT_ELBOW",
-    "LEFT_WRIST",
-    "RIGHT_WRIST",
-    "LEFT_HIP",
-    "RIGHT_HIP",
-    "LEFT_KNEE",
-    "RIGHT_KNEE",
-    "LEFT_ANKLE",
-    "RIGHT_ANKLE",
-    "LEFT_HEEL",
-    "RIGHT_HEEL",
-    "LEFT_FOOT_INDEX",
-    "RIGHT_FOOT_INDEX",
-]
-
+ds['label'] = ds['label'].replace('H', 2)
+ds['label'] = ds['label'].replace('L', 1)
+ds['label'] = ds['label'].replace('C', 0)
+#%% md
+# ### 1.3 Exploratory Data Analysis
 #%%
 ds.columns
-#%% md
-# 
 #%%
-# replacing "H" and "L" values of "label" in the dataset to 0 and "C" values to 1
-
-ds['label'] = ds['label'].replace('H', 0)
-ds['label'] = ds['label'].replace('L', 0)
-ds['label'] = ds['label'].replace('C', 1)
-
+# dataset correlation matrix
 cm = ds.corr()
 
 fig, ax = plt.subplots(figsize=(10, 10))
@@ -66,21 +47,30 @@ plt.show()
 #%%
 ds["label"].value_counts()
 #%%
+# plot the distribution of the target variable
 sns.histplot(ds["label"], kde=True)
+#%% md
+# ### 1.4 Data Modeling
 #%%
 from sklearn.linear_model import LogisticRegression
 
 log_model = LogisticRegression()
 log_model.fit(ds.loc[:, ds.columns != 'label'], ds["label"].values.ravel())
+#%% md
+# ### 1.5 Model Evaluation
 #%%
+# predict the target variable
 y_pred = log_model.predict(ds_test.loc[:, ds_test.columns != 'label'])
 #%%
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 
+# calculate the accuracy of the model
+
 accuracy = accuracy_score(ds_test['label'], y_pred)
 print("Accuracy:", accuracy)
 
+# calculate the confusion matrix
 cm = confusion_matrix(ds_test['label'], y_pred)
 
 fig, ax = plt.subplots(figsize=(10, 6))
@@ -92,7 +82,7 @@ from sklearn.metrics import classification_report
 
 print(classification_report(ds_test['label'], y_pred))
 #%% md
-# 
+# ### 1.6 Feature Scaling
 #%%
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
@@ -118,6 +108,8 @@ _ = plt.ylabel("Actual")
 #%%
 print("Coefficients:", log_model.coef_)
 print("Intercept:", log_model.intercept_)
+#%% md
+# ### 1.7 Feature Importance
 #%%
 import numpy as np
 
@@ -128,6 +120,8 @@ feature_importances = pd.DataFrame(
 ).sort_values(by="coef", ascending=True)
 
 ax.barh(feature_importances["column"], feature_importances["coef"])
+#%% md
+# ### 1.8 Hyperparameter Tuning
 #%%
 from sklearn.model_selection import GridSearchCV
 
@@ -142,5 +136,3 @@ grid.fit(ds.loc[:, ds.columns != 'label'], ds['label'].values.ravel())
 print("Best cross-validation score: {:.2f}".format(grid.best_score_))
 print("Best parameters: ", grid.best_params_)
 
-
-#%%
